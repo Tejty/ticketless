a09133e6dc6134fe1b2fdcaee8d5b1f4eabc0dbd
Tejty <165927081+Tejty@users.noreply.github.com>
2026-03-01 10:21:01 +0100

    Stuff

--- scripts/Talkable.gd
+++ scripts/Talkable.gd
@@ -1,7 +1,16 @@
 class_name Talkable extends Interactable
 
+var talking_time_remaining := 0.0
+
+func is_talking() -> bool:
+	return talking_time_remaining > 0
+
 func interact(by: Node) -> void:
 	say("Hello")
 
-func say(message: String):
+func _physics_process(delta: float) -> void:
+	if !is_talking(): return
+	talking_time_remaining -= delta
+
+static func say(message: String):
 	UiConnector.instance.display_text(message)
--- scripts/death_screen.gd
+++ scripts/death_screen.gd
@@ -8,3 +8,8 @@ func _ready() -> void:
 func _on_player_died(cause: String) -> void:
 	cause_label.text = cause
 	show()
+
+
+func _on_button_pressed() -> void:
+	hide()
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
