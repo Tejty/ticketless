ce8dd7cd4c4e4bf192fd5e9bbabd909871a9f653
Tejty <damian.bonus@gmail.com>
2026-03-07 14:14:21 +0100

    Camera wobble for train in game

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -7,6 +7,9 @@ extends Camera2D
 var _prev_parent: Node
 var _prev_parent_pos: Vector2
 
+var _noise := FastNoiseLite.new()
+var _noise_time := 0.0
+
 func _ready() -> void:
 	_prev_parent = target.get_parent()
 	_prev_parent_pos = _prev_parent.global_position
@@ -21,8 +24,15 @@ func _process(delta: float) -> void:
 
 	# Instantly apply the parent's (train's) movement so the camera rides with it.
 	var parent_pos: Vector2 = parent.global_position
-	position += parent_pos - _prev_parent_pos
+	var parent_velocity := parent_pos - _prev_parent_pos
 	_prev_parent_pos = parent_pos
+	position += parent_velocity
+	
+	var parent_speed := parent_velocity.length_squared()
+	
+	_noise_time += 500 * delta
+	position.x += _noise.get_noise_2d(_noise_time, 0.0) * log(parent_speed + 1) / 10
+	position.y += _noise.get_noise_2d(_noise_time, 500.0) * log(parent_speed + 1) / 10
 
 	# Smoothly lerp for the player's own input-driven movement.
 	position += (target.global_position - Vector2(0, offset_up) - position) * delta * speed
